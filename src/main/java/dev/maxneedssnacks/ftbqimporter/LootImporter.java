64e96c69f829fef2f0071d6a990fd6f8f8e0a8d9
FijiAura <sebastiancatania@icloud.com>
2025-05-13 11:53:19 +0800

    Vibe Coded LootImporter.java

--- src/main/java/dev/maxneedssnacks/ftbqimporter/LootImporter.java
+++ src/main/java/dev/maxneedssnacks/ftbqimporter/LootImporter.java
@@ -1,97 +1,70 @@
-package dev.maxneedssnacks.ftbqimporter;
+package dev.maxneedssnacks.ftbqexporter;
 
+import com.feed_the_beast.ftblib.lib.util.NBTConverter;
 import com.feed_the_beast.ftblib.lib.util.StringUtils;
 import com.feed_the_beast.ftbquests.quest.ServerQuestFile;
 import com.feed_the_beast.ftbquests.quest.loot.LootCrate;
 import com.feed_the_beast.ftbquests.quest.loot.RewardTable;
 import com.feed_the_beast.ftbquests.quest.loot.WeightedReward;
 import com.feed_the_beast.ftbquests.quest.reward.ItemReward;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 
-import javax.annotation.Nullable;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-public class LootImporter {
+public class LootExporter {
 
-    private static LootImporter INSTANCE = null;
+    private final ServerQuestFile questFile;
 
-    private final NBTTagCompound lootFile;
-    private RewardTable crateTable;
-
-    public LootImporter(NBTTagCompound lootFile) {
-        INSTANCE = this;
-        this.lootFile = lootFile;
+    public LootExporter(ServerQuestFile questFile) {
+        this.questFile = questFile;
     }
 
-    public LootImporter(JsonObject lootJson) {
-        INSTANCE = this;
-        this.lootFile = NBTConverter.JSONtoNBT_Object(lootJson, new NBTTagCompound(), true);
-    }
+    public JsonObject exportLoot() {
+        JsonObject lootJson = new JsonObject();
+        JsonArray groupsArray = new JsonArray();
 
-    public boolean processLoot(ServerQuestFile f) {
-        boolean success = true;
+        for (RewardTable table : questFile.rewardTables) {
+            if (table.lootCrate != null) {
+                JsonObject groupJson = new JsonObject();
+                groupJson.addProperty("name", table.title);
+                groupJson.addProperty("weight", 1); // Default weight
 
-        crateTable = new RewardTable(f);
-        crateTable.id = f.newID();
-        f.rewardTables.add(crateTable);
-        crateTable.title = "Loot Chest";
-        crateTable.lootCrate = new LootCrate(crateTable);
-        crateTable.lootCrate.stringID = "loot_chest";
-        crateTable.lootCrate.glow = true;
+                JsonArray rewardsArray = new JsonArray();
+                for (WeightedReward reward : table.rewards) {
+                    if (reward.reward instanceof ItemReward) {
+                        ItemReward itemReward = (ItemReward) reward.reward;
+                        JsonObject rewardJson = new JsonObject();
+                        rewardJson.addProperty("weight", reward.weight);
 
-        for (NBTBase groupBase : lootFile.getTagList("groups", 10)) {
-            success &= processLootGroup((NBTTagCompound) groupBase, f);
-        }
-        return success;
-    }
+                        JsonArray itemsArray = new JsonArray();
+                        JsonObject itemJson = new JsonObject();
+                        NBTTagCompound itemTag = new NBTTagCompound();
+                        itemReward.getItem().writeToNBT(itemTag);
+                        itemJson.addProperty("item", itemTag.toString());
+                        itemsArray.add(itemJson);
 
-    public boolean processLootGroup(NBTTagCompound lootGroup, ServerQuestFile f) {
-        RewardTable table = new RewardTable(f);
-        table.title = lootGroup.getString("name");
-        table.id = f.newID();
-        f.rewardTables.add(table);
-        table.lootCrate = new LootCrate(table);
-        table.lootCrate.stringID = StringUtils.getID(table.title, StringUtils.FLAG_ID_DEFAULTS);
-        table.lootCrate.itemName = table.title;
+                        rewardJson.add("items", itemsArray);
+                        rewardsArray.add(rewardJson);
+                    }
+                }
 
-        crateTable.rewards.add(new WeightedReward(new ItemReward(crateTable.fakeQuest, table.lootCrate.createStack()), lootGroup.getInteger("weight")));
-
-        for (NBTBase rewardBase : lootGroup.getTagList("rewards", 10)) {
-            NBTTagCompound rewardNbt = (NBTTagCompound) rewardBase;
-            List<ItemStack> items = StreamSupport.stream(rewardNbt.getTagList("items", 10).spliterator(), false)
-                    .map(NBTTagCompound.class::cast)
-                    .map(Utils::nbtItem)
-                    .filter(s -> !s.isEmpty())
-                    .collect(Collectors.toList());
-            if (items.isEmpty()) {
-                FTBQImporter.LOGGER.warn("Empty reward in loot group {}!", lootGroup.getString("name"));
-                return false;
-            } else if (items.size() == 1) {
-                table.rewards.add(new WeightedReward(new ItemReward(table.fakeQuest, items.get(0)), rewardNbt.getInteger("weight")));
-            } else {
-                RewardTable table1 = new RewardTable(f);
-                table1.id = f.newID();
-                table1.title = "Nested Reward " + table1.id;
-                items.forEach(stack -> table1.rewards.add(new WeightedReward(new ItemReward(table1.fakeQuest, stack), 1)));
-                table1.lootSize = table1.rewards.size();
-                table1.lootCrate = new LootCrate(table1);
-                table1.lootCrate.stringID = StringUtils.getID(table1.id, StringUtils.FLAG_ID_DEFAULTS);
+                groupJson.add("rewards", rewardsArray);
+                groupsArray.add(groupJson);
             }
         }
-        return true;
+
+        lootJson.add("groups", groupsArray);
+        return lootJson;
     }
 
-    @Nullable
-    public static LootImporter get() {
-        return INSTANCE;
-    }
-
-    public RewardTable getTable() {
-        return crateTable;
+    public NBTTagCompound exportLootToNBT() {
+        JsonObject lootJson = exportLoot();
+        return NBTConverter.JSONtoNBT_Object(lootJson, new NBTTagCompound(), true);
     }
 }
